e796e33a9ec27ff653df4b0413b34281d500de15
Etienne Lord <etienne_lord@hotmail.com>
2019-08-09 16:52:29 -0400

    Added demonstration of 1D CNN, encoding and GRU

---
+++ Day2/Detecting DNA sites/README.md
@@ -0,0 +1,15 @@
+Tutorial on CNN¸and RNN for DNA sequences analysis
+==================================================
+
+This directory includes files for the *Practical examples - Translation RNN and time-series anomaly detection.*
+
+This tutorial is in two parts.
+
+File dpsites.py describe a 1D CNN used to detect cleavage site in DNA.
+
+File dpsites_GRU describe the use of a GRU to accomplish the same task, but with better accuracy.
+
+This code is a modification of code provided in:
+
+Zou, J., Huss, M., Abid, A., Mohammadi, P., Torkamani, A., & Telenti, A. (2018). A primer on deep learning in genomics. Nat Genet. 2019 Jan;51(1):12-18. doi: 10.1038/s41588-018-0295-5.
+
---
+++ Day2/Detecting DNA sites/dpsites_GRU.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python
+#
+# Original code from:  
+# Zou, J., Huss, M., Abid, A., Mohammadi, P., Torkamani, A., & Telenti, A. (2018). A primer on deep learning in genomics. 
+# Nat Genet. 2019 Jan;51(1):12-18. doi: 10.1038/s41588-018-0295-5.
+#
+# Modification: Etienne Lord 2019
+#
+# See also for review: Yue, T., & Wang, H. (2018). Deep learning for genomics: A concise overview. arXiv preprint arXiv:1802.00810.
+# https://arxiv.org/abs/1802.00810
+#
+# Identify some potential Plum pox virus DNA cleaving sites.
+# File: sequences.txt (Cleavage site)
+#       labels.txt    (Information about each site: 0 negative, 1 positive)
+#       test.fasta    (Sample fasta file)
+#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from Bio import SeqIO
+import sklearn
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.layers import Conv1D, Dense, MaxPooling1D, Flatten, GRU
+from tensorflow.keras.models import Sequential
+import tensorflow.keras.backend as K
+from sklearn.metrics import confusion_matrix
+import itertools
+
+################################################################################ 
+# PROCESS THE SITES INFORMATIONS                                               #
+################################################################################
+
+sequence_file = open("sequences.txt", "r")
+sequences = sequence_file.read().split('\n')
+sequences = list(filter(None, sequences))  # This removes empty sequences.
+for i in range(len(sequences)):    
+    sequences[i]=sequences[i].split('\t')[1]
+
+pd.DataFrame(sequences, index=np.arange(1, len(sequences)+1),columns=['Sequences']).head()
+
+################################################################################ 
+# ENCODE THE DNA INFORMATION USING ONE HOT ENCODING                            #
+################################################################################
+# The LabelEncoder encodes a sequence of bases as a sequence of integers.
+
+integer_encoder = LabelEncoder()
+one_hot_encoder = OneHotEncoder(categories=[range(40)])   
+input_features = []
+for sequence in sequences:
+  integer_encoded = integer_encoder.fit_transform(list(sequence))
+  integer_encoded = np.array(integer_encoded).reshape(-1, 1)
+  one_hot_encoded = one_hot_encoder.fit_transform(integer_encoded)
+  input_features.append(one_hot_encoded.toarray())
+
+input_features = np.stack(input_features)
+
+################################################################################ 
+# LOAD LABELS FOR EACH SITES AND ENCODE                                        #
+################################################################################
+label_file = open("labels.txt", "r")
+
+labels = label_file.read().split('\n')
+labels = list(filter(None, labels))  # removes empty sequences
+for i in range(len(labels)):    
+    labels[i]=labels[i].split('\t')[1]
+
+one_hot_encoder2 = OneHotEncoder(categories=[range(2)])
+labels = np.array(labels).reshape(-1, 1)
+input_labels = one_hot_encoder2.fit_transform(labels).toarray()
+
+## SPLIT THE TRAINING DATA 75% TRAINING / 25% VALIDATION
+train_features, test_features, train_labels, test_labels = train_test_split(input_features, input_labels, test_size=0.25, random_state=42)
+
+################################################################################ 
+# MODEL DEFINITION (GRU, one layer)                                            #
+################################################################################
+model = Sequential()
+model.add(GRU(32, dropout=0.01, return_sequences=True, input_shape=(train_features.shape[1], 40)))
+model.add(Flatten())
+model.add(Dense(16, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', 
+              metrics=['binary_accuracy'])
+model.summary()
+
+
+################################################################################ 
+# RUN MODEL                                                                    #
+################################################################################
+history = model.fit(train_features, train_labels,epochs=50, verbose=1, validation_split=0.25)
+
+
+predicted_labels = model.predict(np.stack(test_features))
+cm = confusion_matrix(np.argmax(test_labels, axis=1), 
+                      np.argmax(predicted_labels, axis=1))
+print('Confusion matrix:\n',cm)
+
+
+################################################################################ 
+# TEST ON REAL DATA (test.fasta)                                               #
+################################################################################
+## Now scanning the sequence
+def load_fasta(fa_file):
+    # Variable data 
+    data = []
+
+    # Open the sequences file
+    for record in SeqIO.parse(fa_file, "fasta"):
+            data.append([record.id, record.seq.upper(), None])
+    # Return data
+    return data
+
+proteome=load_fasta("test.fasta")
+df=pd.DataFrame(columns=['name','sequence','probs','max_score'])
+file = open("results_GRU.txt","w") 
+
+## Predict for each sequence of len 9 bp
+for i in range(len(proteome)):
+    seqname=proteome[i][0]
+    print('[ %d/%d %s]'%(i,len(proteome),seqname))
+    seq=str(proteome[i][1])
+    seqlist=list()
+    by=2
+    for j in range(0,len(seq),by):
+        to_test=seq[j:(j+9)]
+        seqlist.append(to_test)       
+        #input_sequence=list()
+    seq_features = []
+    for sequence in seqlist:            
+            if len(sequence)==9:
+                integer_encoded = integer_encoder.fit_transform(list(sequence))
+                integer_encoded = np.array(integer_encoded).reshape(-1, 1)
+                one_hot_encoded = one_hot_encoder.fit_transform(integer_encoded)
+                #Remove the end of sequence            
+                seq_features.append(one_hot_encoded.toarray())  
+    if len(seq_features)>1:
+        predicted_labels = model.predict(np.stack(seq_features))    
+        file.write("%s\t%f\t%s\t%s\n"%(seqname,np.max(predicted_labels[:,1]),seq,','.join(map(str, predicted_labels[:,1]))))
+file.close()
+################################################################################ 
+# FINAL NOTES                                                                  #
+################################################################################
+#
+# 1. This is a modification of the 1D CNN approach with memory cells (GRU)
+# 2. A modification could imply the use of LSTM and longer sequence lengths.
